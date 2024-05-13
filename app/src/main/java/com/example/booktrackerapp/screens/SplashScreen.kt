41a6fb3452cf0256f143a8946711e8d4439f03b2
Nour-hash <113339425+Nour-hash@users.noreply.github.com>
2024-05-13 21:05:46 +0200

    SplashScreen done

--- app/src/main/java/com/example/booktrackerapp/screens/SplashScreen.kt
+++ app/src/main/java/com/example/booktrackerapp/screens/SplashScreen.kt
@@ -1,28 +1,48 @@
 package com.example.booktrackerapp.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.booktrackerapp.R
 import com.example.booktrackerapp.viewModel.SplashViewModel
 import kotlinx.coroutines.delay
 
-private const val SPLASH_TIMEOUT = 1000L
+private const val SPLASH_TIMEOUT = 2000L
 
 @Composable
-fun SplashScreen(navController: NavController,
-                 viewModel: SplashViewModel = hiltViewModel())
-{
+fun SplashScreen(navController: NavController, viewModel: SplashViewModel = hiltViewModel()) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Display your logo image
+        Image(
+            painter = painterResource(id = R.drawable.fabbic_logo),
+            contentDescription = "Logo",
+            modifier = Modifier.size(200.dp),
+            contentScale = ContentScale.Fit
+        )
 
-    Column() {
-        CircularProgressIndicator(color = MaterialTheme.colorScheme.onBackground)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Display circular progress indicator
+        CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
     }
 
-
-
+    // Navigate after a delay
     LaunchedEffect(true) {
         delay(SPLASH_TIMEOUT)
         viewModel.onAppStart(navController)
